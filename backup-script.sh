c9d708d7b10cf2100ff79bd4fc9735e68e0a0a92
hramkumar <ramkumar.harish@gmail.com>
2021-09-30 21:20:24 +0530

    First Commit
    Backup shell script to take backup of logs and archive them.

---
+++ backup-script.sh
@@ -0,0 +1,32 @@
+# $ mkdir -p /tmp/scripts/logs
+# $ cd /tmp/scripts/logs
+# $ touch ansible.log apache.log mysql.log nagios.log
+
+# $ vim backup_files.txt
+# apache.log
+# mysql.log
+# nagios.log
+# ansible.log
+# chef.log
+
+#!/bin/bash
+LOG_DIR='/tmp/scripts/logs'
+BACKUP_DIR='/tmp/scripts/logs_backup'
+mkdir -p $BACKUP_DIR
+for files in $(cat backup_files.txt)
+do
+	if [-f $LOG_DIR/$files]
+	then
+		echo "Copying $files to logs_backup directory.."
+		cp $LOG_DIR/$files $BACKUP_DIR
+	else
+		echo "$files log file already exist, skipping.."
+	fi
+done
+echo ""
+echo ""
+echo "Archiving log files.."
+tar -czvf logs_backup.tgz logs_backup
+echo ""
+echo ""
+echo "Backup completed successfully.."
